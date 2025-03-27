e76b9c8efc811128635c8ebb08281e23df833449
Ouchinitsu <ouchinitsu@gmail.com>
2025-03-27 10:09:34 +0900

    fixed:jsの修正とコードの整形

--- src/main/resources/static/js/technical-dropdown.js
+++ src/main/resources/static/js/technical-dropdown.js
@@ -1,15 +1,15 @@
 // dropdownButtonを押下時にmenuが表示される
-document.getElementById("technicalDropdownButton").addEventListener("click",  ()=> {
-    let menu = document.getElementById("technicalDropdownMenu");
-    menu.classList.toggle("hidden");
+document.getElementById("technicalDropdownButton").addEventListener("click", () => {
+	const menu = document.getElementById("technicalDropdownMenu");
+	menu.classList.toggle("hidden");
 });
 
 // dropdownButtonとmenuではないものを押下時にmenuを隠す
-document.addEventListener("click",  (event)=> {
-    let menu = document.getElementById("technicalDropdownMenu");
-    let button = document.getElementById("technicalDropdownButton");
-    if (!menu.contains(event.target) && event.target !== button) {
+document.addEventListener("click", (event) => {
+	const menu = document.getElementById("technicalDropdownMenu");
+	const button = document.getElementById("technicalDropdownButton");
+	if (!menu.contains(event.target) && event.target !== button) {
 		//classにhiddenを追加
-        menu.classList.add("hidden");
-    }
+		menu.classList.add("hidden");
+	}
 });
