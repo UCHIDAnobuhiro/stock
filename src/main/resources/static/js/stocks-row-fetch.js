ac3646d65f0376faf72c6b9e02d8505455cb1b08
Ouchinitsu <ouchinitsu@gmail.com>
2025-04-01 15:36:10 +0900

    fixed:お気に入りボタンを押下後、銘柄情報更新できなくなるバグを修正

--- src/main/resources/static/js/stocks-row-fetch.js
+++ src/main/resources/static/js/stocks-row-fetch.js
@@ -1,29 +1,33 @@
 import { setSymbol, renderCharts } from './candlestick.js';
-
+//お気に入りボタンを押下時　TRはinnerHTMLで一部更新され、DOMがリセットされeventlistenerがなくなる
+//そのために　TRの親TBにlistenerを追加する
 document.addEventListener('DOMContentLoaded', () => {
-
-	document.querySelectorAll('.clickable-row').forEach(row => {
-		row.addEventListener('click', (event) => {
-			// チェックボックスは無視
-			if (!event.target.closest('input')) {
-				const ticker = row.dataset.ticker;
-				const brand = row.dataset.brand;
-				console.log('クリックされた銘柄:', ticker, brand);
-				setSymbol(ticker);
-				renderCharts();
-
-				// 銘柄情報を非同期で取得して挿入
-				fetch(`/stock/table?symbol=${encodeURIComponent(ticker)}`)
-					.then(res => res.text())
-					.then(html => {
-						console.log('取得したHTML:', html); // ← ここで確認！
-						document.getElementById('todayInformation').innerHTML = html;
-						document.getElementById('tickerNameAndCode').innerText = `${brand} (${ticker})`;
-					})
-					.catch(err => {
-						console.error('銘柄情報取得失敗:', err);
-					});
-			}
-		});
-	});
+	const stocksTB = document.querySelector("#stocksTB");
+	if (stocksTB) {
+	    stocksTB.addEventListener('click', onRowClick);
+	}
 });
+
+const onRowClick = (event) => {
+	//clickable-rowの押下時だけ動く
+	const row = event.target.closest('.clickable-row');
+
+	// チェックボックスは無視
+	if (!row || event.target.closest('input')) return;
+	const ticker = row.dataset.ticker;
+	const brand = row.dataset.brand;
+	console.log('クリックされた銘柄:', ticker, brand);
+	setSymbol(ticker);
+	renderCharts();
+
+	// 銘柄情報を非同期で取得して挿入
+	fetch(`/stock/table?symbol=${encodeURIComponent(ticker)}`)
+		.then(res => res.text())
+		.then(html => {
+			console.log('取得したHTML:', html); // ← ここで確認！
+			document.getElementById('todayInformation').innerHTML = html;
+			document.getElementById('tickerNameAndCode').innerText = `${brand} (${ticker})`;
+		})
+		.catch(err =>
+			console.error('銘柄情報取得失敗:', err));
+};
