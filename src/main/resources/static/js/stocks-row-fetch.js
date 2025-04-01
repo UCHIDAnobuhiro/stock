227dc77757e8abc8b0c59efbf58752769c054b40
UCHIDAnobuhiro <92204227+UCHIDAnobuhiro@users.noreply.github.com>
2025-04-01 13:15:18 +0900

    feat:当日の銘柄情報を表示

--- src/main/resources/static/js/stocks-row-fetch.js
+++ src/main/resources/static/js/stocks-row-fetch.js
@@ -1,6 +1,7 @@
 import { setSymbol, renderCharts } from './candlestick.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+
 	document.querySelectorAll('.clickable-row').forEach(row => {
 		row.addEventListener('click', (event) => {
 			// チェックボックスは無視
@@ -10,6 +11,18 @@ document.addEventListener('DOMContentLoaded', () => {
 				console.log('クリックされた銘柄:', ticker, brand);
 				setSymbol(ticker);
 				renderCharts();
+
+				// 銘柄情報を非同期で取得して挿入
+				fetch(`/stock/table?symbol=${encodeURIComponent(ticker)}`)
+					.then(res => res.text())
+					.then(html => {
+						console.log('取得したHTML:', html); // ← ここで確認！
+						document.getElementById('todayInformation').innerHTML = html;
+						document.getElementById('tickerNameAndCode').innerText = `${brand} (${ticker})`;
+					})
+					.catch(err => {
+						console.error('銘柄情報取得失敗:', err);
+					});
 			}
 		});
 	});
