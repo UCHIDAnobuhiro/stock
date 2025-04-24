879a80233150b7bcc6e03ab38562c911aafc8e2c
UCHIDAnobuhiro <92204227+UCHIDAnobuhiro@users.noreply.github.com>
2025-04-24 14:14:11 +0900

    fix:テストの修正

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -14,6 +14,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.example.stock.exception.StockApiException;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -27,6 +28,9 @@ public class StockServiceTest {
 
 	@MockBean
 	private LogoDetectionService mockLogoDetectionService;
+
+	@MockBean
+	private ImageAnnotatorClient imageAnnotatorClient;
 
 	@BeforeEach
 	void clearCache() {
