ccf64581105da646f50aa99373d9bd3db7a557ae
UCHIDAnobuhiro <92204227+UCHIDAnobuhiro@users.noreply.github.com>
2025-05-07 16:46:55 +0900

    fix:時刻の変更

--- src/main/java/com/example/stock/service/StockBatchService.java
+++ src/main/java/com/example/stock/service/StockBatchService.java
@@ -29,7 +29,7 @@ public class StockBatchService {
 			"1week", List.of(13, 26, 52),
 			"1month", List.of(9, 24, 60));
 
-	@Scheduled(cron = "0 45 16  * * *", zone = "Asia/Tokyo")
+	@Scheduled(cron = "0 50 16  * * *", zone = "Asia/Tokyo")
 	public void runRateLimitedBatch() {
 		log.info("=== バッチ処理開始 ===");
 		List<String> symbols = tickersRepository.findAllTickers();
