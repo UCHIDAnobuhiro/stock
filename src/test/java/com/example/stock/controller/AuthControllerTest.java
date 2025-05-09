ae2a9367dc5dcf2f61348f0f3c3641085d235a6c
UCHIDAnobuhiro <92204227+UCHIDAnobuhiro@users.noreply.github.com>
2025-05-09 11:38:53 +0900

    fix:テストの修正

--- src/test/java/com/example/stock/controller/AuthControllerTest.java
+++ src/test/java/com/example/stock/controller/AuthControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,7 +22,7 @@ public class AuthControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 
-	@Autowired
+	@MockBean
 	private OtpService otpService;
 
 	@Autowired
